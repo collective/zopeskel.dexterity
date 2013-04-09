8e03a9d02a87dceaf60e65f21c790e5f220d12d3
Steve McMahon <steve@dcn.org>
2013-04-09 11:07:41 -0700

    Change readme to .rst for nice github display

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.5.0'
 setup(name='zopeskel.dexterity',
       version=version,
       description="Paster templates for dexterity",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
