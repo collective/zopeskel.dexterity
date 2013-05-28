ddfa6ce35c1198f5de7cc295dff90d693110021a
Steve McMahon <steve@dcn.org>
2013-05-28 16:39:02 -0700

    Ask the question

--- zopeskel/dexterity/dexterity.py
+++ zopeskel/dexterity/dexterity.py
@@ -2,7 +2,23 @@ import copy
 
 from zopeskel.plone import BasicZope
 from zopeskel.base import get_var
-from zopeskel.base import var
+from zopeskel.base import EASY, EXPERT
+from zopeskel.vars import BooleanVar
+
+dex_vars = [
+    BooleanVar('grokish',
+        title='Grok-Based?',
+        description="True/False: Use grok conventions to simplify coding?",
+        modes=(EASY, EXPERT),
+        default=True,
+        help="""
+Grok is a convention-over-configuration framework that simplifies
+many aspects of the Zope Component Architecture. Grok makes it easier
+to learn and use Dexterity, but it is not part of Plone's core.
+""",
+        ),
+    ]
+
 
 class Dexterity(BasicZope):
     _template_dir = 'templates/dexterity'
@@ -14,6 +30,7 @@ class Dexterity(BasicZope):
     use_local_commands = True
     use_cheetah = True
     vars = copy.deepcopy(BasicZope.vars)
+    vars += dex_vars
     get_var(vars, 'namespace_package').default = 'plone'
     get_var(vars, 'package').default = 'example'
     get_var(vars, 'description').default = 'Example Dexterity Product'
